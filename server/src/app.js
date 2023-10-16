7e9d789e0b2e5905594f7109ee213750691e61bf
Aswin Gangadharan <aswin.gangadharan.2020@gmail.com>
2023-10-16 16:06:58 +0530

    Add error handler

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,10 @@
+import 'express-async-errors'
+
 import express from 'express'
 import morgan from 'morgan'
 import cors from 'cors'
+
+import { notfound, errorHandler } from './middlewares/error.js'
 
 const app = express()
 
@@ -9,5 +13,8 @@ app.use(morgan('dev'))
 app.use(cors())
 
 app.get('/', (req, res) => res.send('Hello world'))
+
+app.use(notfound)
+app.use(errorHandler)
 
 export default app
---
+++ server/src/middlewares/error.js
@@ -0,0 +1,21 @@
+import { AppError } from '../utils/app-error.js'
+
+export async function notfound(req, res) {
+  res.status(400).json({
+    message: 'Url not found',
+  })
+}
+
+export async function errorHandler(err, req, res, next) {
+  console.error(err)
+
+  if (err instanceof AppError) {
+    return res.status(err.statusCode).json({
+      message: err.message,
+    })
+  }
+
+  res.status(500).json({
+    message: 'Internal server error',
+  })
+}
