0cc9926f5ff2a6eae1a0d019535fdb85a913321c
Aswin Gangadharan <aswin.gangadharan.2020@gmail.com>
2023-10-16 18:06:15 +0530

    Add signin form

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from 'react-router-dom'
 import HomePage from './pages/home'
 import SignupPage from './pages/signup'
+import SigninPage from './pages/signin'
 
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="/signup" element={<SignupPage />} />
+      <Route path="/signin" element={<SigninPage />} />
     </Routes>
   )
 }
---
+++ client/src/pages/signin.jsx
@@ -0,0 +1,63 @@
+import { useState } from 'react'
+import { useNavigate, Navigate } from 'react-router-dom'
+
+import api from '../api'
+import useUser from '../hooks/user'
+import AuthLayout from '../layouts/auth'
+
+export default function SigninPage() {
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [loading, setLoading] = useState(false)
+
+  const navigate = useNavigate()
+  const { user, setUser } = useUser()
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+
+    try {
+      const res = await api.post('/auth/signin', {
+        username,
+        password,
+      })
+      setUser(res.data.user)
+      navigate('/')
+    } catch (err) {
+      console.error(err)
+    }
+  }
+
+  if (user) {
+    return <Navigate to="/" />
+  }
+
+  return (
+    <AuthLayout>
+      <h1 className="fw-bold text-center mb-3">Signin</h1>
+
+      <div className="col-3 mx-auto">
+        <form onSubmit={handleSubmit}>
+          <input
+            className="form-control mb-3"
+            type="text"
+            placeholder="Username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+
+          <input
+            className="form-control mb-3"
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+
+          <button className="btn btn-primary">Submit</button>
+        </form>
+        <hr />
+      </div>
+    </AuthLayout>
+  )
+}
