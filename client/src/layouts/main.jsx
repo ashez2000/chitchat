210421dd198592163aae46f11a95209808707a9b
Aswin Gangadharan <aswin.gangadharan.2020@gmail.com>
2023-10-16 18:35:59 +0530

    Protect homepage

--- client/src/layouts/main.jsx
+++ client/src/layouts/main.jsx
@@ -1,3 +1,28 @@
+import { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import api from '../api'
+import useUser from '../hooks/user'
+
 export default function MainLayout(props) {
-  return <div>{props.children}</div>
+  const [loading, setLoading] = useState(true)
+  const { setUser } = useUser()
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    api
+      .get('/auth/profile')
+      .then((res) => {
+        setUser(res.data.user)
+        setLoading(false)
+      })
+      .catch((err) => {
+        navigate('/signin')
+      })
+  }, [])
+
+  if (loading) {
+    return <div className="container">Loading...</div>
+  }
+
+  return <main className="container">{props.children}</main>
 }
--- client/src/pages/home.jsx
+++ client/src/pages/home.jsx
@@ -1,3 +1,5 @@
+import MainLayout from '../layouts/main'
+
 export default function HomePage() {
-  return <div>HomePage</div>
+  return <MainLayout>HomePage</MainLayout>
 }
