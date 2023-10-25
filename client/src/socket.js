3dc55897976e1cdd54cd14bddaedae004af76685
Aswin Gangadharan <aswin.gangadharan.2020@gmail.com>
2023-10-25 23:35:00 +0530

    fix: socket io url in client

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,3 +1,6 @@
 import { io } from 'socket.io-client'
 
-export const socket = io('http://localhost:3000')
+const API_URL =
+  import.meta.env.MODE === 'production' ? '/' : 'http://localhost:3000'
+
+export const socket = io(API_URL)
