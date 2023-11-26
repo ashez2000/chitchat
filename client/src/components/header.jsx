26ca5f8cfbfb786defce115a7a96d64ea7d95656
Aswin Gangadharan <aswin.gangadharan.2020@gmail.com>
2023-11-26 06:41:02 +0530

    Refactor header

--- client/src/components/header.jsx
+++ client/src/components/header.jsx
@@ -1,26 +1,18 @@
-import { Link } from 'react-router-dom'
-import useUser from '../hooks/user'
 import { signout } from '../services/auth'
 
-export default function Header() {
-  const { user, setUser } = useUser()
-
+export default function Header({ username }) {
   const handleSignout = async () => {
-    setUser(null)
     await signout()
   }
 
   return (
     <header className="d-flex justify-content-between align-items-center my-3">
       <div className="d-flex gap-3">
-        <Link to="/">SimpleChat</Link>
-        <span>Signed in as {user && `@${user.username}`}</span>
+        <span>Signed in as @{username}</span>
       </div>
-      {user && (
-        <button className="btn btn-sm btn-secondary" onClick={handleSignout}>
-          Signout
-        </button>
-      )}
+      <button className="btn btn-sm btn-secondary" onClick={handleSignout}>
+        Signout
+      </button>
     </header>
   )
 }
--- client/src/layouts/main.jsx
+++ client/src/layouts/main.jsx
@@ -2,7 +2,6 @@ import { Navigate } from 'react-router-dom'
 import useUser from '../hooks/user'
 
 import Header from '../components/header'
-import SideNav from '../components/sidenav'
 import { useEffect } from 'react'
 import { socket } from '../socket'
 
@@ -23,14 +22,9 @@ export default function MainLayout(props) {
 
   return (
     <main className="container">
-      <Header />
+      <Header username={user.username} />
       <hr />
-      <div className="row gx-3">
-        <div className="col-3">
-          <SideNav />
-        </div>
-        <div className="col-9">{props.children}</div>
-      </div>
+      {props.children}
     </main>
   )
 }
