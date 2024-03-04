8f40eba17e25a1df59109266eac70a96ab2fc676
Aswin Gangadharan <aswin.gangadharan.2020@gmail.com>
2024-03-04 21:39:14 +0530

    Update users seed data

--- server/seed/main.js
+++ server/seed/main.js
@@ -1,26 +1,50 @@
 import { PrismaClient } from '@prisma/client'
+import argon from 'argon2'
+
 const db = new PrismaClient()
 
-async function main() {
-  await db.user.create({
-    data: {
-      id: 'clntfb4b50000ukbk8obx8lri',
-      name: 'Aswin',
-      username: 'aswin',
-      password: '123456',
-    },
-  })
+const users = [
+  { name: 'John Doe', username: 'john' },
+  { name: 'Tom Langosh', username: 'tom' },
+  { name: 'Alexys Schumm', username: 'alexys' },
+  { name: 'Myrtice Lebsack', username: 'myrtice' },
+  { name: 'Lelia Turner', username: 'lelia' },
+  { name: 'Jovany Goodwin', username: 'jovany' },
+  { name: 'Vanessa Deckow', username: 'vanessa' },
+  { name: 'Janelle Renner', username: 'janelle' },
+  { name: 'Kevin Lubowitz', username: 'kevin' },
+  { name: 'Loy Howell', username: 'loy' },
+  { name: 'Tad Bogisich', username: 'tad' },
+  { name: 'Kelvin Grady', username: 'kelvin' },
+  { name: 'Candida Wolff', username: 'candida' },
+  { name: 'Hildegard Schroeder', username: 'hildegard' },
+  { name: 'Lysanne Keeling', username: 'lysanne' },
+  { name: 'Meghan Cartwright', username: 'meghan' },
+  { name: 'Evangeline Will', username: 'evangeline' },
+  { name: 'Orpha Berge', username: 'orpha' },
+  { name: 'Rosetta Harvey', username: 'rosetta' },
+  { name: 'Fidel Emmerich', username: 'fidel' },
+  { name: 'Taurean Crona', username: 'taurean' },
+]
 
-  await db.user.create({
-    data: {
-      id: 'clntfb4ba0001ukbkwugfwl10',
-      name: 'James',
-      username: 'james',
-      password: '123456',
-    },
-  })
+const main = async () => {
+  await db.user.deleteMany()
 
-  await db.$disconnect()
+  const password = await argon.hash('123456')
+
+  const usersPromises = users.map((u) =>
+    db.user.create({
+      data: {
+        name: u.name,
+        username: u.username,
+        password,
+      },
+    })
+  )
+
+  await Promise.all(usersPromises)
+
+  console.log('Database seeded')
 }
 
 main()
