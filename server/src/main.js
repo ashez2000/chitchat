64cc68827164db5b944a6038523d544e57406a41
Aswin Gangadharan <aswin.gangadharan.2020@gmail.com>
2024-03-27 20:17:50 +0530

    Add user repository

--- server/src/main.js
+++ server/src/main.js
@@ -1,4 +1,9 @@
+import { v4 as uuid } from 'uuid'
 import * as repo from './repository/mod.js'
 import { migrate, db } from './db/mod.js'
 
 migrate()
+
+const res = repo.user.search('')
+
+console.log(res)
--- server/src/repository/user.js
+++ server/src/repository/user.js
@@ -2,7 +2,9 @@ import { v4 as uuid } from 'uuid'
 import bcrypt from 'bcryptjs'
 import { db } from '../db/mod.js'
 
-/** Create new user */
+/**
+ * Create new user
+ */
 export const create = ({ username, password }) => {
   const id = uuid()
   const hash = bcrypt.hashSync(password)
@@ -10,4 +12,31 @@ export const create = ({ username, password }) => {
   return db
     .prepare('INSERT INTO users (id, username, password) VALUES (?, ?, ?)')
     .run(id, username, hash)
+}
+
+/**
+ * Find user by id
+ */
+export const findById = (id) => {
+  return db
+    .prepare('SELECT id, username, password FROM users WHERE id = ?')
+    .get(id)
+}
+
+/**
+ * Find user by username
+ */
+export const findByUsername = (username) => {
+  return db
+    .prepare('SELECT id, username, password FROM users WHERE username = ?')
+    .get(username)
+}
+
+/**
+ * Search for users by partial matching username
+ */
+export const search = (username) => {
+  return db
+    .prepare('SELECT id, username, password FROM users WHERE username LIKE ?')
+    .all(`%${username}%`)
 }
