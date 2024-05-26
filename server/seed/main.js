ca9a32efe8f712445bce2e017ef385d3bcfa847d
Aswin Gangadharan <aswin.gangadharan.2020@gmail.com>
2024-05-26 15:50:50 +0530

    Update seeds and migration

--- server/seed/main.js
+++ server/seed/main.js
@@ -4,11 +4,13 @@ import bcrypt from 'bcryptjs'
 import Database from 'better-sqlite3'
 import { v4 as uuid } from 'uuid'
 
+import { migrate } from '../src/db/mod.js'
+import users from './users.js'
+
 const db = new Database(process.env.DATABASE_URL)
 
-const users = ['john', 'tom', 'kevin', 'loy', 'tad', 'kelvin', 'james', 'aswin']
-
 const main = async () => {
+  migrate()
   const password = bcrypt.hashSync('123456')
 
   const sql = db.prepare(
