4fe378b21ae9edd1c82c1a528e6ffc4fb2b1e49c
Aswin Gangadharan <aswin.gangadharan.2020@gmail.com>
2024-05-26 10:46:07 +0530

    Add user search input

--- client/src/components/user-chats.jsx
+++ client/src/components/user-chats.jsx
@@ -1,20 +1,34 @@
 import { Link } from 'react-router-dom'
-import useSwr from 'swr'
 
 import * as api from '../api/mod'
 import { useEffect, useState } from 'react'
 
 export default function UserChats() {
   const [users, setUsers] = useState([])
+  const [searchTerm, setSearchTerm] = useState('')
 
   useEffect(() => {
-    api.user.search().then(setUsers).catch(console.error)
-  }, [])
+    const timeoutId = setTimeout(
+      () => api.user.search(searchTerm).then(setUsers).catch(console.error),
+      300
+    )
+
+    return () => clearTimeout(timeoutId)
+  }, [searchTerm])
 
   return (
     <div className="d-flex flex-column gap-3">
+      <div>
+        <input
+          className="form-control"
+          type="search"
+          placeholder="search users ..."
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+      </div>
+
       {users.map((u) => (
-        <div className="">
+        <div className="" key={u.id}>
           - <Link to={`/chats/${u.id}`}>{u.username}</Link>
         </div>
       ))}
