84cb60154ca92d27dc9a7da058e3855aff480d9f
Aswin Gangadharan <aswin.gangadharan.2020@gmail.com>
2024-10-05 10:37:32 +0530

    Refactor chat and message to mongoose

--- server/src/controllers/chat.js
+++ server/src/controllers/chat.js
@@ -1,27 +1,28 @@
-import * as repo from '../repository/mod.js'
+import { Chat } from '../model/chat.js'
+import { Message } from '../model/message.js'
 
 /**
  * Get messages between users
  * @route GET /api/chats/:userId
  */
-export const getMessages = (req, res) => {
+export async function getMessages(req, res) {
   const curUserId = req.user.id
   const userId = req.params.userId
 
   // If chat doen't exist return empty array
-  const chatId = repo.chat.find([curUserId, userId])
-  if (!chatId) {
+  const chat = await Chat.findOne({ users: { $all: [curUserId, userId] } })
+  if (!chat) {
     return res.status(200).json({
       messages: [],
       chatId: null,
     })
   }
 
-  const messages = repo.message.find(chatId)
+  const messages = await Message.find({ chat: chat.id }).sort({ createdAt: 'desc' })
 
   res.status(200).json({
     messages,
-    chatId,
+    chatId: chat.id,
   })
 }
 
@@ -29,20 +30,19 @@ export const getMessages = (req, res) => {
  * Create new message between two users
  * @route POST /api/chats/:userId
  */
-export const createMessage = (req, res) => {
+export async function createMessage(req, res) {
   const curUserId = req.user.id
   const userId = req.params.userId
   const content = req.body.content
 
-  // If chat not found create new chat
-  const chatId = repo.chat.find([curUserId, userId])
-  console.log('createMessage CTRL: ChatId =', chatId)
-  if (!chatId) {
-    const newChatId = repo.chat.create([curUserId, userId])
-    const message = repo.message.create(content, newChatId, curUserId)
+  // if chat does not exist create new chat between users
+  let chat = await Chat.findOne({ users: { $all: [curUserId, userId] } })
+  if (!chat) {
+    chat = await Chat.create({ users: [curUserId, userId] })
+    const message = await Message.create({ content, chat: chat.id, user: curUserId })
     return res.status(201).json(message)
   }
 
-  const message = repo.message.create(content, chatId, curUserId)
+  const message = await Message.create({ content, chat: chat.id, user: curUserId })
   res.status(201).json(message)
 }
